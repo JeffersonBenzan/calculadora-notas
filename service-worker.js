bb68a4c87422ae954297b4d2ac9ddc811fb85882
Jefferson Benzan Tapia <jeffersonbt20@gmail.com>
2025-02-14 15:13:23 -0400

    add pwa

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "calculadora-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/favicon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cache => cache !== CACHE_NAME).map(cache => caches.delete(cache))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
